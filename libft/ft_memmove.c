15b2bc6dc1aba11e0e0d5f1c32362e1b1c6d6b45
Mina Kim <mmmiiinn@me.com>
2023-01-06 02:14:15 +0100

    05.2023

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -16,11 +16,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
 	const unsigned char	*s;
-	size_t				i;
 
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	i = 0;
+
 	if (!src && !dest)
 		return (NULL);
 	if (d > s)
